ee1772c264b556c9bbacb4754c2f9adc7ac8393a
ale <rimoldi@ditoy.com>
2017-09-07 17:26:58 +0200

    locally defning the import works

--- eval-file-pyqt5-set-value/input-number.py
+++ eval-file-pyqt5-set-value/input-number.py
@@ -1,14 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QInputDialog
 
 class App():
 
     def __init__(self):
+        from PyQt5.QtWidgets import QInputDialog
         num,ok = QInputDialog.getInt(None, "integer input dualog", "enter a number")
         if ok:
             pass 
 
 if __name__ == '__main__':
+    from PyQt5.QtWidgets import QApplication
     # app = QApplication(sys.argv)
-    app = QApplication([])
+    app = QApplication(['myname'])
     ex = App()
