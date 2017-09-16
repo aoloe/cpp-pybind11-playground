ede7deb5e2d1256dc1beb5c27445e69dd7c843cb
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-09-15 20:11:44 -0400

    Simpler, still crashes

--- scripter-class/src/main.cpp
+++ scripter-class/src/main.cpp
@@ -2,21 +2,30 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/embed.h>
 
-#include "scripter.h"
 #include "foo.h"
 
 namespace py = pybind11;
-
 using namespace pybind11::literals;
 
-void run()
+void runFile(std::string fileName)
 {
-    Scripter scripter1;
-    scripter1.runFile("../python/set-bar.py");
+    py::scoped_interpreter guard{};
+    
+    Foo foo1, foo2;
+    
+    auto module = py::module::import("fooapi");
+    
+    auto locals = py::dict("foo_copy"_a=foo1,
+                            "foo_ref"_a=py::cast(foo2, py::return_value_policy::reference)); // foo1 by value, foo2 by reference
+    
+    //py::eval_file(fileName, py::globals(), locals);
+    
+    //assert(foo1.bar == 1);
+    //assert(foo2.bar == 5);
 }
 
 int main()
 {
-    run();
-    run();
+    runFile("../python/set-bar.py");
+    runFile("../python/set-bar.py");
 }
