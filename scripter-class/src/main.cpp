23313dcfbe42ad74506cb22b9b1d1b11a6e5e9a3
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-09-15 21:13:57 -0400

    cleaner and simpler

--- scripter-class/src/main.cpp
+++ scripter-class/src/main.cpp
@@ -7,25 +7,18 @@
 namespace py = pybind11;
 using namespace pybind11::literals;
 
-void runFile(std::string fileName)
-{
-    py::scoped_interpreter guard{};
-    
-    Foo foo1, foo2;
-    
-    auto module = py::module::import("fooapi");
-    
-    auto locals = py::dict("foo_copy"_a=foo1,
-                            "foo_ref"_a=py::cast(foo2, py::return_value_policy::reference)); // foo1 by value, foo2 by reference
-    
-    //py::eval_file(fileName, py::globals(), locals);
-    
-    //assert(foo1.bar == 1);
-    //assert(foo2.bar == 5);
+int main() {
+    for(int i=0; i<2; i++) {
+        py::scoped_interpreter guard{};
+        
+        Foo foo1, foo2;
+        
+        auto module = py::module::import("fooapi");
+        
+        auto locals = py::dict("foo_copy"_a=foo1,
+                                "foo_ref"_a=py::cast(foo2, py::return_value_policy::reference)); // foo1 by value, foo2 by reference
+        
+        std::cout << "Once" << std::endl;
+    }
 }
 
-int main()
-{
-    runFile("../python/set-bar.py");
-    runFile("../python/set-bar.py");
-}
