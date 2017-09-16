1f35030a6c5a1fe7edacb404ae793f928a954259
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-09-15 21:56:27 -0400

    Smaller

--- scripter-class/src/foo.h
+++ scripter-class/src/foo.h
@@ -1,4 +1,9 @@
+#include <iostream>
 struct Foo {
     int bar = 1;
+
+    Foo() {std::cout << "Making a foo!" << std::endl;}
+    Foo(const Foo& a) {bar = a.bar; std::cout << "Copying a new foo" << std::endl;}
+    ~Foo() {std::cout << "Bye-bye foo!" << std::endl;}
 };
 
--- scripter-class/src/main.cpp
+++ scripter-class/src/main.cpp
@@ -11,12 +11,12 @@ int main() {
     for(int i=0; i<2; i++) {
         py::scoped_interpreter guard{};
         
-        Foo foo1, foo2;
+        Foo foo;
         
         auto module = py::module::import("fooapi");
         
-        auto locals = py::dict("foo_copy"_a=foo1,
-                                "foo_ref"_a=py::cast(foo2, py::return_value_policy::reference)); // foo1 by value, foo2 by reference
+        auto locals = py::dict("foo_copy"_a=foo);
+        // Fails also: "foo_ref"_a=py::cast(foo, py::return_value_policy::reference)); // foo1 by value, foo2 by reference
         
         std::cout << "Once" << std::endl;
     }
