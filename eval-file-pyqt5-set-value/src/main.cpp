352de41fca7115e0e64ac04b71ad2120d9ed1776
ale rimoldi <ale@graphicslab.org>
2019-02-20 11:50:49 +0100

    no tabs, please

--- eval-file-pyqt5-set-value/src/main.cpp
+++ eval-file-pyqt5-set-value/src/main.cpp
@@ -14,14 +14,14 @@ class Dog
 
 int main()
 {
-	py::scoped_interpreter guard{};
+    py::scoped_interpreter guard{};
 
-	auto local = py::dict();
+    auto local = py::dict();
 
-	int val_out;
-	local["set_the_answer"] = py::cpp_function([&](int value) { val_out = value; });
+    int val_out;
+    local["set_the_answer"] = py::cpp_function([&](int value) { val_out = value; });
 
     py::eval_file("input-number.py", py::globals(), local);
 
-	std::cout << "value set by the python script: " << val_out << std::endl;
+    std::cout << "value set by the python script: " << val_out << std::endl;
 }
