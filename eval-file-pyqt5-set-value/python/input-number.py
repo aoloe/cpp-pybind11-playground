efb6b1c3b21e7de039b99b951435b913eb1c5f60
Gaël Écorchard <gael.ecorchard@cvut.cz>
2020-06-29 10:21:36 +0200

    Fix eval-file-pyqt5-set-value

--- eval-file-pyqt5-set-value/python/input-number.py
+++ eval-file-pyqt5-set-value/python/input-number.py
@@ -1,14 +1,10 @@
-import sys
-
-def get_the_value(locals):
+def get_the_value(locals_):
     from PyQt5.QtWidgets import QInputDialog
-    num,ok = QInputDialog.getInt(None, "The number to be set", "enter a number")
+    num, ok = QInputDialog.getInt(None, 'The number to be set', 'enter a number')
     if ok:
-        locals["set_the_answer"](num)
-        set_the_answer(num)
+        locals_['set_the_answer'](num)
 
 if __name__ == '__main__':
     from PyQt5.QtWidgets import QApplication
-    # app = QApplication(sys.argv)
     app = QApplication(['myname'])
     get_the_value(locals())
--- eval-file-pyqt5-set-value/src/main.cpp
+++ eval-file-pyqt5-set-value/src/main.cpp
@@ -3,15 +3,6 @@
 
 namespace py = pybind11;
 
-class Dog
-{
-    public:
-        void setName(std::string n) { name = n; }
-        std::string getName() { return name; }
-    private:
-        std::string name;
-};
-
 int main()
 {
     py::scoped_interpreter guard{};
