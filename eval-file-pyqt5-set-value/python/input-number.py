c2a3d9a7a83b5cc39eba74c8dcc613f05ac086d7
ale rimoldi <ale@graphicslab.org>
2020-07-02 10:02:19 +0200

    add again the other options

--- eval-file-pyqt5-set-value/python/input-number.py
+++ eval-file-pyqt5-set-value/python/input-number.py
@@ -1,8 +1,11 @@
 def get_the_value(outer_locals):
     from PyQt5.QtWidgets import QInputDialog
+    # locals().update(outer_locals) # a way to make the locals difference explicit
     num, ok = QInputDialog.getInt(None, 'The number to be set', 'enter a number')
     if ok:
         outer_locals['set_the_answer'](num)
+        # locals()['set_the_answer'](num)
+        # set_the_answer(num) # this cannot work (see README)
 
 if __name__ == '__main__':
     from PyQt5.QtWidgets import QApplication
